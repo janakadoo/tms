508f062de0ca05c441eeffb872e0fa2041da1673
janakadoo <janaka.ravs@gmail.com>
2026-07-08 10:32:01 +0530

    Feat: Add View Attachment button in Expense details modal

--- js/modules/expenses.js
+++ js/modules/expenses.js
@@ -276,6 +276,13 @@ export const ExpenseModule = {
                             ${Utils.esc(e.description || 'No remarks provided.')}
                         </div>
                     </div>
+                    ${e.attachment_id ? `
+                    <div style="margin-top:1rem;">
+                        <button class="btn btn-secondary" onclick="document.querySelector('[data-view-att=\\'${e.attachment_id}\\']').click()">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" style="width:16px;height:16px;vertical-align:middle;margin-right:0.25rem;"><path stroke-linecap="round" stroke-linejoin="round" d="M18.375 12.739l-7.693 7.693a4.5 4.5 0 01-6.364-6.364l10.94-10.94A3 3 0 1119.5 7.372L8.552 18.32m.009-.01l-.01.01m5.699-9.941l-7.81 7.81a1.5 1.5 0 002.112 2.13"/></svg>
+                            View Attachment
+                        </button>
+                    </div>` : ''}
                 `;
                 Utils.viewDialog('Expense Details', html);
             };
